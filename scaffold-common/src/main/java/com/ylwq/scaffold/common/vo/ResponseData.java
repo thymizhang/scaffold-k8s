c41293c1f15448e237cd675ebbbaa62895818340
thymi <13607822999@163.com>
2021-02-01 18:38:24 +0800

    依赖优化

--- scaffold-common/src/main/java/com/ylwq/scaffold/common/vo/ResponseData.java
+++ scaffold-common/src/main/java/com/ylwq/scaffold/common/vo/ResponseData.java
@@ -1,7 +1,6 @@
 package com.ylwq.scaffold.common.vo;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
