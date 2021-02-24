7ef77c0429f4a5d08b520fe76d406b4c61ab6a4a
thymi <13607822999@163.com>
2021-02-24 17:24:57 +0800

    代码优化

--- scaffold-service-user/src/main/java/com/ylwq/scaffold/service/user/UserApplication.java
+++ scaffold-service-user/src/main/java/com/ylwq/scaffold/service/user/UserApplication.java
@@ -3,7 +3,6 @@ package com.ylwq.scaffold.service.user;
 import com.ylwq.scaffold.common.util.JvmUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 用户服务
