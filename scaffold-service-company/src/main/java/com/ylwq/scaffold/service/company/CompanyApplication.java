f5bfabead896b443734c6118c1dc05e775e2f449
thymi <13607822999@163.com>
2021-03-11 19:00:41 +0800

    新增计划任务

--- scaffold-service-company/src/main/java/com/ylwq/scaffold/service/company/CompanyApplication.java
+++ scaffold-service-company/src/main/java/com/ylwq/scaffold/service/company/CompanyApplication.java
@@ -4,6 +4,7 @@ import com.ylwq.scaffold.common.util.JvmUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 公司服务
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableFeignClients
+@EnableScheduling
 public class CompanyApplication {
     public static void main(String[] args) {
         SpringApplication.run(CompanyApplication.class, args);
