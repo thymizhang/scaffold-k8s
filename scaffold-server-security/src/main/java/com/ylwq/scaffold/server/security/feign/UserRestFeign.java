f4a6c7d75103a9e18a0d2f7f0d0daf35081a9fac
thymi <13607822999@163.com>
2021-04-26 17:43:00 +0800

    服务调用从配置获取name、url参数

--- scaffold-server-security/src/main/java/com/ylwq/scaffold/server/security/feign/UserRestFeign.java
+++ scaffold-server-security/src/main/java/com/ylwq/scaffold/server/security/feign/UserRestFeign.java
@@ -8,14 +8,14 @@ import org.springframework.cloud.openfeign.FeignClient;
  * 技术要点：
  * 1. 需要在MainClass加上注解：`@EnableFeignClients`;<br/>
  * 2. 基于Springboot注册中心调用写法<br/>
- * `@FeignClient(value = "service-user")`<br/>
+ * `@FeignClient(name = "${service.user.provider.name}")`<br/>
  * 3. 基于Kubernetes调用写法<br/>
- * `@FeignClient(name = "service-user", url = "http://user:7001`<br/>
- * url可以在放在配置中心配置，url = "${rpc.service.user.url}"<br/>
+ * `@FeignClient(name = "${service.user.provider.name}", url = "${service.user.provider.url}")`<br/>
+ * name和url在配置文件中配置<br/>
  *
  * @Author thymi
  * @Date 2021/3/9
  */
-@FeignClient(value = "service-user")
+@FeignClient(name = "${service.user.provider.name}", url = "${service.user.provider.url}")
 public interface UserRestFeign extends UserRestApi {
 }
